6cd89568bcd794c68406c6b2ea9200a8cc113e28
insomnia <jutao@ihealthlabs.com.cn>
2017-07-27 14:09:00 +0800

    init

---
+++ app/src/main/java/com/insomnia/testtravisci/MainActivity.java
@@ -0,0 +1,12 @@
+package com.insomnia.testtravisci;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
