13aa8af2da239e63fb00474c1fc6bfbd7f167ecd
insomnia <jutao@ihealthlabs.com.cn>
2017-07-27 14:18:29 +0800

    add 1st log

--- app/src/main/java/com/insomnia/testtravisci/MainActivity.java
+++ app/src/main/java/com/insomnia/testtravisci/MainActivity.java
@@ -2,11 +2,14 @@ package com.insomnia.testtravisci;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.e("Main", "hello world");
     }
 }
